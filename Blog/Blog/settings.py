15a046f35020b063af34ac34fd61ed09fc19d263
gowmarcisrael-cyber <gowmarcisrael@gmail.com>
2026-09-12 16:01:35 +0000

    Integration de Claudinary

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'Post',
     'User',
 ]
@@ -139,4 +141,13 @@ MAILERS = {
 MEDIA_URL ='media/'
 MEDIA_ROOT = BASE_DIR/'media/'
 AUTH_USER_MODEL = 'User.User'
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+if not DEBUG: 
+    CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
