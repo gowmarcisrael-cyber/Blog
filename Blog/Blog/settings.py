c77ba193c330d22005f387db765a327a00a1369e
gowmarcisrael-cyber <gowmarcisrael@gmail.com>
2026-09-12 16:59:02 +0000

    Fix: Utilisation du dictionnaire STORAGES pour Cloudinary

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -150,4 +150,12 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
